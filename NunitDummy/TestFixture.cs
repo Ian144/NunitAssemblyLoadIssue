b57588e1c03768d4f34a45adf0738c04c1b3f52d
Ian144 <ian144@hotmail.com>
2021-04-15 10:22:21 +0100

    Add project files.

---
+++ NunitDummy/TestFixture.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+ using NUnit.Framework;
+
+ namespace NunitDummy
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [Test]
+        public void Test1() => Assert.Fail();
+
+        [Test] 
+        public void Test2() => Assert.Pass();
+    }
+}
